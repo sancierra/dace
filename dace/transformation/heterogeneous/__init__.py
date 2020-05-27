0db7fc0f50e989b7e20c061d844918c673c9c28e
luca <sancierra@gmail.com>
2020-05-27 12:08:07 +0200

    heterogeneous __init__

--- dace/transformation/heterogeneous/__init__.py
+++ dace/transformation/heterogeneous/__init__.py
@@ -1,3 +1,6 @@
 """ Initialize heterogeneous transformations """
 
 from .reduce_map import ReduceMap
+from .subgraph_fusion import SubgraphFusion
+from .expansion import MultiExpansion
+import helpers
