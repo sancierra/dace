268d29b6b8d999166442522e2c29f1f4be70d5ef
luca <sancierra@gmail.com>
2020-05-29 14:26:01 +0200

    pipeline and smax run tests

--- dace/transformation/heterogeneous/smax.py
+++ dace/transformation/heterogeneous/smax.py
@@ -13,6 +13,8 @@ import dace.libraries.standard as stdlib
 
 import timeit
 
+import pipeline
+
 dace_dtype = dace.float32
 H, B, SN, SM = (dace.symbol(s) for s in ('H', 'B', 'SN', 'SM'))
 
@@ -42,34 +44,20 @@ def softmax(X_in: dace_dtype[H, B, SN, SM]):
 
     return out
 
-
-if __name__ == '__main__':
-    sdfg = softmax.to_sdfg()
-    sdfg.view()
-    roofline = Roofline(PERF_GPU_DAVINCI, symbols = {H:30, B:30, SN:300, SM:300})
-    graph = sdfg.nodes()[0]
-
-    sdfg.save("softmax0")
-    ################ first, expand the reduce node
+def expand_reduce(sdfg, graph):
     reduce_nodes = []
     for node in graph.nodes():
         if isinstance(node, stdlib.Reduce):
             reduce_nodes.append(node)
 
-
-    sdfg.view()
     trafo_reduce = ReduceMap(0,0,{},0)
     start = timeit.default_timer()
     for reduce_node in reduce_nodes:
         trafo_reduce.expand(sdfg,graph,reduce_node)
     end = timeit.default_timer()
     print("***** Reduction timer =",end-start,"s")
-    sdfg.view()
 
-    sdfg.save("softmax1")
-
-
-    ############### second, do MultiExpansion
+def expand_maps(sdfg, graph):
     trafo_expansion = MultiExpansion()
     map_entries = []
     for node in graph.nodes():
@@ -80,20 +68,67 @@ if __name__ == '__main__':
     end = timeit.default_timer()
     print("***** Expansion timer =",end-start,"s")
 
-    sdfg.view()
-
-    sdfg.save("softmax2")
-
-    ############ third, do MapFusion
+def subgraph_fusion(sdfg, graph, map_entries):
     map_fusion = SubgraphFusion()
     start = timeit.default_timer()
     map_fusion.fuse(sdfg, graph, map_entries)
     end = timeit.default_timer()
     print("***** MapFusion timer =",end-start,"s")
 
+
+
+sdfg = softmax.to_sdfg()
+roofline = Roofline(PERF_GPU_DAVINCI, symbols = {H:5, B:5, SN:100, SM:100})
+graph = sdfg.nodes()[0]
+H.set(5); B.set(5); SN.set(100); SM.set(100)
+
+def test_graph():
+    ################ first, expand the reduce node
+    pipeline.expand_reduce(sdfg, graph)
+    sdfg.view()
+
+    ############### second, do MultiExpansion
+    pipeline.expand_maps(sdfg, graph)
+    sdfg.view()
+
+    ############ third, do MapFusion
+    pipeline.fusion(sdfg, graph)
+
     sdfg.apply_strict_transformations()
     sdfg.view()
 
-    sdfg.save("softmax3")
-
     sdfg.validate()
+
+def test_result():
+    X_in = np.random.rand(H.get(), B.get(), SN.get(), SM.get()).astype(np.float32)
+    X_out_baseline = np.zeros([H.get(), B.get(), SN.get(), SM.get()], dtype = np.float32)
+    X_out_1 = np.zeros([H.get(), B.get(), SN.get(), SM.get()], dtype = np.float32)
+    X_out_2 = np.zeros([H.get(), B.get(), SN.get(), SM.get()], dtype = np.float32)
+    X_out_3 = np.zeros([H.get(), B.get(), SN.get(), SM.get()], dtype = np.float32)
+
+    #csdfg = sdfg.compile_directly()
+    #X_out_baseline = csdfg(X_in = X_in, H=H, B=B, SN=SN, SM=SM)
+
+    pipeline.expand_reduce(sdfg, graph)
+    sdfg.expand_library_nodes()
+    sdfg.view()
+
+    csdfg = sdfg.compile_directly()
+    X_out_1 = csdfg(X_in = X_in, H=H, B=B, SN=SN, SM=SM)
+
+    pipeline.expand_maps(sdfg, graph)
+
+    csdfg = sdfg.compile_directly()
+    X_out_2 = csdfg(X_in = X_in, H=H, B=B, SN=SN, SM=SM)
+
+    pipeline.fuse(sdfg, graph)
+
+    csdfg = sdfg.compile_directly()
+    X_out_3 = csdfg(X_in = X_in, H=H, B=B, SN=SN, SM=SM)
+
+    print("Diff1", np.norm(X_out_baseline - X_out_1))
+    print("Diff2", np.norm(X_out_baseline - X_out_2))
+    print("Diff3", np.norm(X_out_baseline - X_out_3))
+
+if __name__ == "__main__":
+    test_result()
