091e63cfca3c78844c96783397f7daef64d9cbd3
luca <sancierra@gmail.com>
2020-06-30 12:06:24 +0200

    reduction test

--- dace/transformation/heterogeneous/tests/reduction.py
+++ dace/transformation/heterogeneous/tests/reduction.py
@@ -2,7 +2,9 @@ import dace
 import numpy as np
 
 from dace.transformation.heterogeneous.pipeline import expand_reduce, expand_maps, fusion
+from dace.transformation.heterogeneous.reduce.cuda_block import CUDABlockAllReduce
 
+from dace.libraries.standard.nodes.reduce import Reduce
 N = dace.symbol('N')
 M = dace.symbol('M')
 
@@ -15,7 +17,7 @@ def TEST(A: dace.float32[M,N]):
     return dace.reduce(lambda a, b: max(a,b), A, axis=1, identity = 0)
 
 
-@ dace.program
+@dace.program
 def TEST2(A: dace.float32[M,N]):
     tmp_out = np.ndarray([M], dace.float32)
     for i in dace.map[0:M]:
@@ -23,41 +25,35 @@ def TEST2(A: dace.float32[M,N]):
     return tmp_out
 
 
-
 A = np.random.rand(M.get(), N.get()).astype(np.float32)
 
 
-
 if __name__ == '__main__':
-    sdfg1 = TEST.to_sdfg()
-    sdfg2 = TEST2.to_sdfg()
-    sdfg1.apply_gpu_transformations()
-    sdfg2.apply_gpu_transformations()
+    sdfg = TEST.to_sdfg()
+    graph = sdfg.nodes()[0]
+    for node in graph.nodes():
+        if isinstance(node, Reduce):
+            reduce_node = node
 
+    reduce_node.implementation = 'CUDA (block)'
+    # first, check whether we can apply:
+    check = CUDABlockAllReduce.can_be_applied(graph,
+                                     {CUDABlockAllReduce._reduce: graph.nodes().index(reduce_node)},
+                                      0, sdfg)
+    print(check)
+    sdfg_id = 0
+    state_id = 0
+    subgraph = {CUDABlockAllReduce._reduce: graph.nodes().index(reduce_node)}
+    transformation = CUDABlockAllReduce(sdfg_id, state_id, subgraph, 0)
+    #sdfg.view()
+    transformation.apply(sdfg)
+    sdfg.view()
+
+    '''
+    ## sdfg2 does not work here
     #return1 = sdfg1.compile()(A=A, N=N, M=M)
     #return2 = sdfg2.compile()(A=A, N=N, M=M)
 
     #print(np.linalg.norm(return1))
     #print(np.linalg.norm(return2))
-
-    ###################
-    sdfg1.expand_library_nodes()
-    sdfg2.expand_library_nodes()
-    
-    
-    for sdfg in [sdfg2]:
-        for node in sdfg.nodes()[0].nodes():
-            if isinstance(node, dace.sdfg.nodes.NestedSDFG):
-                for state in node.sdfg.nodes():
-                    for snode in state.nodes():
-                        for e in state.out_edges(snode):
-                            e.data.wcr_conflict = False
-                            #if isinstance(snode, dace.sdfg.nodes.MapEntry):
-                            #    snode.schedule = dace.dtypes.ScheduleType.Sequential
-    
-    
-    
-    return1 = sdfg1.compile()(A=A, N=N, M=M)
-    return2 = sdfg2.compile()(A=A, N=N, M=M)
-    print(np.linalg.norm(return1))
-    print(np.linalg.norm(return2))
+    '''
