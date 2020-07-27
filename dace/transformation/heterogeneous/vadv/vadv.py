d67e47c34934870880fe68daa71d81bcf9903745
luca <sancierra@gmail.com>
2020-07-27 23:00:01 +0200

    vadv

--- dace/transformation/heterogeneous/vadv/vadv.py
+++ dace/transformation/heterogeneous/vadv/vadv.py
@@ -10,23 +10,23 @@ import dace.transformation.heterogeneous as heterogeneous
 vadv_unfused = SDFG.from_file('vadv-input.sdfg')
 # apply state fusion exhaustively
 vadv_unfused.apply_transformations_repeated(StateFusion)
-vadv_unfused.view()
-
 vadv_fused_partial = SDFG.from_file('vadv-2part.sdfg')
-vadv_fused_partial.view()
-
 vadv_fused_full = SDFG.from_file('vadv-fused.sdfg')
-vadv_fused_full.view()
+
+def view_all():
+    vadv_unfused.view()
+    vadv_fused_partial.view()
+    vadv_fused_full.view()
 
 
 def test_matching():
     graph = vadv_unfused.nodes()[0]
-    nodes = [node for node in graph.nodes()]
-    subgraph1 = dace.sdfg.graph.SubgraphView(graph, nodes)
-    subgraph2 = dace.sdfg.graph.SubgraphView(graph, [graph.nodes()[0],graph.nodes()[1]])
+    # should yield True
+    subgraph = dace.sdfg.graph.SubgraphView(graph, [node for node in graph.nodes()])
     expansion = heterogeneous.MultiExpansion()
-    print("True  ==",expansion.match(vadv_unfused,subgraph1))
-    print("False ==",expansion.match(vadv_unfused,subgraph2))
+    print("True  ==", heterogeneous.MultiExpansion.match(vadv_unfused,subgraph))
+    fusion = heterogeneous.SubgraphFusion()
+    print("True  ==", heterogeneous.SubgraphFusion.match(vadv_unfused,subgraph))
 
-
+#view_all()
 test_matching()
