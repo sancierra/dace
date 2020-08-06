e48fbc9ccfbad9f95b2d40c0af7cb3283b8bbc68
luca <lucalav@student.ethz.ch>
2020-08-06 16:49:00 +0200

    reduction pass davinci

--- dace/transformation/subgraph/tests/reduction.py
+++ dace/transformation/subgraph/tests/reduction.py
@@ -1,39 +1,32 @@
 import dace
 import numpy as np
 
-from dace.transformation.subgraph.pipeline import expand_reduce, expand_maps, fusion
-from dace.transformation.subgraph.reduce.cuda_block import CUDABlockAllReduce
-
 from dace.transformation.subgraph import ReduceExpansion
 
 from dace.libraries.standard.nodes.reduce import Reduce
+
 N = dace.symbol('N')
 M = dace.symbol('M')
-
-
 N.set(300); M.set(300)
-
 
 @dace.program
 def TEST(A: dace.float32[M,N]):
     return dace.reduce(lambda a, b: max(a,b), A, axis=1, identity = 0)
 
 
-A = np.random.rand(M.get(), N.get()).astype(np.float32)
-
-
-if __name__ == '__main__':
+def test():
+    A = np.random.rand(M.get(), N.get()).astype(np.float32)
     sdfg = TEST.to_sdfg()
     sdfg.apply_gpu_transformations()
 
-    # numerical test
-    result1 = sdfg(A=A)
-    #
     graph = sdfg.nodes()[0]
     for node in graph.nodes():
         if isinstance(node, Reduce):
             reduce_node = node
-    sdfg.view()
+    reduce_node.implementation = 'CUDA (device)'
+
+    csdfg = sdfg.compile()
+    result1 = csdfg(A=A,M=M,N=N)
     print(reduce_node)
 
     sdfg_id = 0
@@ -41,20 +34,17 @@ if __name__ == '__main__':
     subgraph = {ReduceExpansion._reduce: graph.nodes().index(reduce_node)}
     # expand first
     transform = ReduceExpansion(sdfg_id, state_id, subgraph, 0)
-    transform.cuda_expand = False
-    transform.reduce_implementation = 'CUDA (block)'
+    transform.reduce_implementation = 'CUDA (block allreduce)'
     transform.apply(sdfg)
-    sdfg.view()
+    transform._new_reduce.implementation = 'CUDA (block allreduce)'
+    csdfg = sdfg.compile()
+    result2 = csdfg(A=A,M=M,N=N)
 
-    for node in graph.nodes():
-        if isinstance(node, Reduce):
-            reduce_node = node
-    # first, check whether we can apply:
-    check = CUDABlockAllReduce.can_be_applied(graph,
-                                     {CUDABlockAllReduce._reduce: graph.nodes().index(reduce_node)},
-                                      0, sdfg)
-    print(check)
-    subgraph = {CUDABlockAllReduce._reduce: graph.nodes().index(reduce_node)}
-    transform = CUDABlockAllReduce(sdfg_id, state_id, subgraph, 0)
-    transform.apply(sdfg)
-    sdfg.view()
+    assert np.allclose(result1, result2)
+
+    print("PASS")
+
+
+if __name__ == '__main__':
+    test()
+
