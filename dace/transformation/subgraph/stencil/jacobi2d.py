aa5627f05d20c7e76656bba006eb41018882fa9a
luca <sancierra@gmail.com>
2020-08-11 13:54:56 +0200

    jacobi testing

--- dace/transformation/subgraph/stencil/jacobi2d.py
+++ dace/transformation/subgraph/stencil/jacobi2d.py
@@ -1,4 +1,10 @@
 import dace
+from dace.transformation.dataflow.tiling import MapTiling
+from dace.transformation.subgraph.pipeline import fusion
+from dace.sdfg.propagation import propagate_memlets_sdfg
+
+import dace.subsets as subsets
+
 try:
     import polybench
 except ImportError:
@@ -68,8 +74,54 @@ def init_array(A, B):  #, N, tsteps):
             B[i, j] = datatype(i * (j + 3) + 3) / n
 
 
+
+def pre_tiling(sdfg, tile_size = 64):
+    graph = None
+    for g in sdfg.nodes():
+        if len(g.nodes()) == 9:
+            graph = g
+            break
+
+    for node in graph.nodes():
+        if isinstance(node, dace.sdfg.nodes.MapEntry):
+            if node.label == 'a':
+                entry1 = node
+            if node.label == 'b':
+                entry2 = node
+
+    # hack: modify B[i,j] memlet to B[i-1,j-1]
+    for node in graph.nodes():
+        if isinstance(node, dace.sdfg.nodes.CodeNode):
+            if node.label == 'a':
+                edge = graph.out_edges(node)[0]
+                edge.data.subset.offset(subsets.Indices.from_string('1,1'), negative = True)
+    propagate_memlets_sdfg(sdfg)
+
+    print(f"Operating on graph {graph}")
+    print(f"with map entries {entry1} and {entry2}")
+
+    d1 = {MapTiling._map_entry: graph.nodes().index(entry1)}
+    d2 = {MapTiling._map_entry: graph.nodes().index(entry2)}
+
+    t1 = dace.transformation.dataflow.tiling.MapTiling(sdfg.sdfg_id,
+         sdfg.nodes().index(graph), d1, 0)
+    t2 = dace.transformation.dataflow.tiling.MapTiling(sdfg.sdfg_id,
+         sdfg.nodes().index(graph), d2, 0)
+
+    t1.strides    = (tile_size, tile_size)
+    t1.tile_sizes = (tile_size + 2, tile_size + 2) ## !!
+
+    t2.strides    = (tile_size, tile_size)
+    t2.tile_sizes = (tile_size, tile_size)
+
+    t1.apply(sdfg)
+    t2.apply(sdfg)
+
+
 if __name__ == '__main__':
     #polybench.main(sizes, args, [(0, 'A')], init_array, jacobi2d)
     sdfg = jacobi2d.to_sdfg()
     sdfg.apply_strict_transformations()
+    sdfg.view()
+    pre_tiling(sdfg)
     sdfg.view()
