58915e52e9eb9013f8f467a9837e35e4fa276ec2
luca <sancierra@gmail.com>
2020-08-23 23:06:52 +0200

    late night changes

--- dace/transformation/subgraph/subgraph_fusion.py
+++ dace/transformation/subgraph/subgraph_fusion.py
@@ -40,23 +40,12 @@ class SubgraphFusion(pattern_matching.SubgraphTransformation):
 
     debug = Property(desc="Show debug info", dtype=bool, default=False)
 
-    cpu_transient_allocation = Property(
-        desc="Storage Location to push"
-        "transients to in CPU environment",
+    transient_allocation = Property(
+        desc="Storage Location to push transients to that are "
+             "fully contained within the subgraph.",
         dtype=dtypes.StorageType,
-        default=dtypes.StorageType.Default,
-        choices=[dtypes.StorageType.Register,
-                 dtypes.StorageType.CPU_Heap,
-                 dtypes.StorageType.CPU_ThreadLocal,
-                 dtypes.StorageType.Default])
+        default=dtypes.StorageType.Default)
 
-    cuda_transient_allocation = Property(desc="Storage Location to push"
-                                         "transients to in GPU environment",
-                                         dtype=dtypes.StorageType,
-                                         default=dtypes.StorageType.Default,
-                                         choices=[dtypes.StorageType.Register,
-                                                  dtypes.StorageType.GPU_Shared,
-                                                  dtypes.StorageType.Default])
 
     @staticmethod
     def match(sdfg, subgraph):
@@ -435,15 +424,15 @@ class SubgraphFusion(pattern_matching.SubgraphTransformation):
         return (subgraph_contains_data, transients_created,
                 invariant_dimensions)
 
-    def apply(self, sdfg, subgraph, **kwargs):
+    def apply(self, sdfg, subgraph, do_not_override = [], **kwargs):
         self.subgraph = subgraph
 
         graph = subgraph.graph
 
         map_entries = helpers.get_highest_scope_maps(sdfg, graph, subgraph)
-        self.fuse(sdfg, graph, map_entries, **kwargs)
+        self.fuse(sdfg, graph, map_entries, do_not_override, **kwargs)
 
-    def fuse(self, sdfg, graph, map_entries, **kwargs):
+    def fuse(self, sdfg, graph, map_entries, do_not_override = [], **kwargs):
         """ takes the map_entries specified and tries to fuse maps.
 
             all maps have to be extended into outer and inner map
@@ -459,10 +448,10 @@ class SubgraphFusion(pattern_matching.SubgraphTransformation):
             :param graph: State
             :param map_entries: Map Entries (class MapEntry) of the outer maps
                                 which we want to fuse
-            :param do_not_override: List of AccessNodes that are transient but
-                                  should not be directly modified when pushed
-                                  into the global map. Instead the array
-                                  is taken itself.
+            :param do_not_override: List of data names whose corresponding nodes
+                                    are fully contained within the subgraph
+                                    but should not be augmented/transformed
+                                    nevertheless.
         """
 
         # if there are no maps, return immediately
@@ -552,12 +541,6 @@ class SubgraphFusion(pattern_matching.SubgraphTransformation):
         global_map_entry.schedule = schedule
         graph.add_node(global_map_entry)
         graph.add_node(global_map_exit)
-
-        try:
-            # in-between transients whose data should not be modified
-            do_not_override = kwargs['do_not_override']
-        except KeyError:
-            do_not_override = []
 
         # next up, for any intermediate node, find whether it only appears
         # in the subgraph or also somewhere else / as an input
@@ -802,10 +785,7 @@ class SubgraphFusion(pattern_matching.SubgraphTransformation):
                 transient_to_transform.total_size = new_data_totalsize
                 transient_to_transform.offset = new_data_offset
                 transient_to_transform.lifetime = dtypes.AllocationLifetime.Scope
-                if schedule == dtypes.ScheduleType.GPU_Device:
-                    transient_to_transform.storage = self.cuda_transient_allocation
-                else:
-                    transient_to_transform.storage = self.cpu_transient_allocation
+                transient_to_transform.storage = self.transient_allocation
 
 
             else:
