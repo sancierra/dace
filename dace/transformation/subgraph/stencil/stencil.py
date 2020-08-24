a4111843ff4ba170bf18cd50d172cae7d30a20c1
luca <sancierra@gmail.com>
2020-08-24 16:59:56 +0200

    this would work on the gpu, but not on the cpu -- missing synchronization

--- dace/transformation/subgraph/stencil/stencil.py
+++ dace/transformation/subgraph/stencil/stencil.py
@@ -12,7 +12,32 @@ T = dace.symbol('T')
 datatype = dace.float64
 
 @dace.program
-def stencil2d(A:datatype[N,N],B:datatype[N,N]):
+def stencil2d_nontransient(A:datatype[N,N],B:datatype[N,N]):
+    for t in range(T):
+        @dace.map
+        def a(i: _[1:N-1], j:_[1:N-1]):
+            a1 << A[i, j]
+            a2 << A[i, j - 1]
+            a3 << A[i, j + 1]
+            a4 << A[i + 1, j]
+            a5 << A[i - 1, j]
+            b >> B[i, j]
+
+            b = 2*(a1+a2+a3+a4+a5)
+        @dace.map
+        def b(i: _[2:N-2], j:_[2:N-2]):
+            a1 << B[i, j]
+            a2 << B[i, j - 1]
+            a3 << B[i, j + 1]
+            a4 << B[i + 1, j]
+            a5 << B[i - 1, j]
+            b >> A[i, j]
+
+            b = 2*(a1+a2+a3+a4+a5)
+
+@dace.program
+def stencil2d_transient(A:datatype[N,N]):
+    B = np.ndarray([N,N], dtype = datatype)
     for t in range(T):
         @dace.map
         def a(i: _[1:N-1], j:_[1:N-1]):
@@ -39,7 +64,7 @@ def init_array(A):
     n = N.get()
     for i in range(n):
         for j in range(n):
-            A[i,j] = datatype(i * j + 2)/n
+            A[i,j] = 1.0
 
 
 def pre_tiling(sdfg, graph, tile_size = 64, tile_offsets = (1,1), sequential = False):
@@ -65,18 +90,19 @@ def pre_tiling(sdfg, graph, tile_size = 64, tile_offsets = (1,1), sequential = F
          sdfg.nodes().index(graph), d2, 0)
 
     t1.strides    = (tile_size, tile_size)
-    t1.tile_sizes = (tile_size + 2, tile_size + 2) ## !!
-    t1.strides_offset = tile_offsets
+    t1.tile_sizes = (tile_size +2, tile_size +2) ## !!
+    t1.strides_offset = (1,1)#tile_offsets
 
     t2.strides    = (tile_size, tile_size)
     t2.tile_sizes = (tile_size, tile_size)
+    t2.strides_offset = (1,1)
 
     t1.apply(sdfg)
     t2.apply(sdfg)
 
     if sequential:
         for node in graph.nodes():
-            if isinstance(node,dace.sdfg.nodes.MapEntry) and (node.label == 'a' or node.label == 'b'):
+            if isinstance(node,dace.sdfg.nodes.MapEntry):
                 node.map.schedule = dace.dtypes.ScheduleType.Sequential
 
 
@@ -97,8 +123,20 @@ def evaluate(sdfg, graph, view = False, compile = False):
     return result
 
 
-def run(sdfg, tile_size, view = True, compile = False, gpu = False, sequential = False):
+def run(tile_size, view = True, compile = False, gpu = False, sequential = False, transient = True):
+    if transient:
+        sdfg = stencil2d_transient.to_sdfg()
+    else:
+        sdfg = stencil2d_nontransient.to_sdfg()
+    sdfg.specialize({'N':N})
+    #propagate_memlets_sdfg(sdfg)
+    sdfg.propagate = False
+    TILE_SIZE = 4
+    N.set(13)
+    T.set(1)
+    sdfg.specialize({'N':N})
     sdfg.apply_strict_transformations()
+
     graph = None
     for g in sdfg.nodes():
         if len(g.nodes()) == 9:
@@ -124,17 +162,41 @@ def run(sdfg, tile_size, view = True, compile = False, gpu = False, sequential =
     if compile:
         #print(R1)
         #print(R2)
+        '''
+        print(R1[0:8, 0:8])
+        print(R2[0:8, 0:8])
+        print("--------------------------")
+        print(R1[0:8, 8:16])
+        print(R2[0:8, 8:16])
+        print("--------------------------")
+
+        print(R1[8:16, 0:8])
+        print(R2[8:16, 0:8])
+        print("--------------------------")
+
+        print(R1[8:16, 8:16])
+        print(R2[8:16, 8:16])
+        print("--------------------------")
+
+        print(R1[16:20, 0:8])
+        print(R2[16:20, 0:8])
+        print("--------------------------")
+
+        print(R1[16:20, 8:16])
+        print(R2[16:20, 8:16])
+        print("--------------------------")
+
+        print(R1[16:20, 16:20])
+        print(R2[16:20, 16:20])
+        '''
+
         assert np.allclose(R1,R2)
         assert np.allclose(R1,R3)
 
 
 
 if __name__ == '__main__':
-    sdfg = stencil2d.to_sdfg()
-    #propagate_memlets_sdfg(sdfg)
-    sdfg.propagate = False
     TILE_SIZE = 8
-    N.set(20)
+    N.set(21)
     T.set(1)
-    sdfg.specialize({'N':N})
-    run(sdfg, TILE_SIZE, compile = True, gpu = False, view = False, sequential = True)
+    run(TILE_SIZE, compile = True, gpu = False, view = False, sequential = True, transient = False)
