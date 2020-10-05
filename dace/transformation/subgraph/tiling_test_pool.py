a68f6b88b3188872b56290a37f98047416d146a1
luca <sancierra@gmail.com>
2020-10-05 17:23:57 +0200

    unittests work

--- dace/transformation/subgraph/tiling_test_pool.py
+++ dace/transformation/subgraph/tiling_test_pool.py
@@ -10,83 +10,48 @@ N = dace.variable('N')
 N.set(100)
 
 @dace.program
-def stencil(A: dace.float32[N], B: dace.float32[N]):
+def stencil(A: dace.float32[2*N], B: dace.float32[N]):
     tmp1 = np.ndarray((N), dtype = dace.float32)
     tmp2 = np.ndarray((N), dtype = dace.float32)
     tmp3 = np.ndarray((N), dtype = dace.float32)
 
     @dace.map
-    def m1(i: _[1:N]):
-        in1 << A[i]
-        in2 << A[i-1]
+    def m1(i: _[0:N]):
+        in1 << A[2*i]
+        in2 << A[2*i+1]
         out1 >> tmp1[i]
-        out1 = (in1 - in2)/float(2.0)
+        out1 = (in1 + in2)/float(2.0)
 
     @dace.map
-    def m2(i: _[0:N-1]):
+    def m2(i: _[1:N-1]):
         in1 << A[i]
         in2 << A[i+1]
-        out1 >> tmp2[i]
-        out1 = (in2 - in1)/float(2.0)
-
-    @dace.map
-    def m3(i: _[1:N-1]):
-        in1_1 << tmp1[i]
-        in1_2 << tmp2[i+1]
-
-        in2_1 << tmp2[i]
-        in2_0 << tmp2[i-1]
-
-        out1 >> tmp3[i]
-
-        out1 = in1_1 - float(0.2)*in1_2 + in2_1 + float(0.8)*in2_0
-
-    @dace.map
-    def m4(i: _[2:N-1]):
-        in1 << tmp3[i-1:i+2]
+        in3 << A[i-1]
         out1 >> B[i]
-
-        out1 = in1[1]-0.5*(in1[0] + in2[2])
-
+        out1 = (in2 - 0.2 * in1 - 0.2 * in3)
 
 @dace.program
-def stencil_offset(A: dace.float32[N], B: dace.float32[N]):
+def stencil_offset(A: dace.float32[2*N], B: dace.float32[N]):
     tmp1 = np.ndarray((N), dtype = dace.float32)
     tmp2 = np.ndarray((N), dtype = dace.float32)
     tmp3 = np.ndarray((N), dtype = dace.float32)
 
     @dace.map
-    def m1(i: _[0:N-1]):
+    def m1(i: _[0:N]):
+        in1 << A[2*i]
+        in2 << A[2*i+1]
+        out1 >> tmp1[i]
+        out1 = (in1 + in2)/float(2.0)
+
+    @dace.map
+    def m2(i: _[0:N-2]):
         in1 << A[i+1]
-        in2 << A[i-1+1]
-        out1 >> tmp1[i+1]
-        out1 = (in1 - in2)/float(2.0)
+        in2 << A[i+1+1]
+        in3 << A[i-1+1]
+        out1 >> B[i+1]
+        out1 = (in2 - 0.2 * in1 - 0.2 * in3)
 
-    @dace.map
-    def m2(i: _[0:N-1]):
-        in1 << A[i]
-        in2 << A[i+1]
-        out1 >> tmp2[i]
-        out1 = (in2 - in1)/float(2.0)
 
-    @dace.map
-    def m3(i: _[2:N]):
-        in1_1 << tmp1[i-1]
-        in1_2 << tmp2[i+1-1]
-
-        in2_1 << tmp2[i-1]
-        in2_0 << tmp2[i-1-1]
-
-        out1 >> tmp3[i-1]
-
-        out1 = in1_1 - float(0.2)*in1_2 + in2_1 + float(0.8)*in2_0
-
-    @dace.map
-    def m4(i: _[0:N-1-2]):
-        in1 << tmp3[i-1+2:i+2+2]
-        out1 >> B[i+2]
-
-        out1 = in1[1]-0.5*(in1[0] + in2[2])
 
 def test_stencil(tile_size, offset = False):
 
